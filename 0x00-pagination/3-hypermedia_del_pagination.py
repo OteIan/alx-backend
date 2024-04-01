0517f916a9dce8a7eb3d6f5d235de59045a92ba3
Rich_ar_Dev <moriasrichar3@gmail.com>
2024-04-01 21:12:53 +0300

    Deletion-resilient

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -8,6 +8,13 @@ import math
 from typing import List, Dict
 
 
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Helper function
+    """
+    return (page_size * (page - 1), page_size * page)
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -39,14 +46,46 @@ class Server:
             }
         return self.__indexed_dataset
 
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Simple pagination
+        """
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+
+        start, end = index_range(page, page_size)
+
+        data = self.dataset()
+
+        if start > len(data):
+            return []
+        return data[start:end]
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Deletion-resilient hypermedia pagination
         """
-        total_pages = len(self.__dataset) // page_size
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+
+        page_data = []
+        data_count = 0
+        nxt_index = None
+        start = index if index else 0
+
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
+                data_count += 1
+                continue
+
+            if data_count == page_size:
+                nxt_index = i
+                break
+
         return {
-                'index': index,
-                'next_index': None,
-                'page_size': page_size,
-                'data': None
+            'index': index,
+            'next_index': nxt_index,
+            'page_size': len(page_data),
+            'data': page_data
         }
