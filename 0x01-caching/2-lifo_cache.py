eec1360d849be275884c0b02cd504fd13ee9f046
OteIan <oteian9@gmail.com>
2024-04-03 11:32:15 +0300

    ..

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -22,7 +22,7 @@ class LIFOCache(BaseCaching):
             return
 
         if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 poppedItem = self.cache_data.popitem(True)
                 print(f"DISCARD: {poppedItem[0]}")
 
