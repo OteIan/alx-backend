526386e2f8ef45e3ce4dc8ae4e2bb0b74d70ca56
OteIan <oteian9@gmail.com>
2024-04-03 23:43:51 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+MRU Caching
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Assigns items to the dictionary
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.cache_data.popitem(True)
+                print(f"DISCARD: {lru_key[0]}")
+
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns the value of key
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=True)
+        return self.cache_data.get(key, None)
