5b1d278b7814c6e9bb1696315df207ea29944323
OteIan <oteian9@gmail.com>
2024-04-09 14:00:55 +0300

    ..

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def basic_route() -> str:
+def basic_route():
     """
     Basic route
     """
@@ -16,4 +16,4 @@ def basic_route() -> str:
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
