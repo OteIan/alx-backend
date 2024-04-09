6d2a5a0fe1b54bf2c64c176155ebc6571cff4ec3
OteIan <oteian9@gmail.com>
2024-04-09 13:59:08 +0300

    Fix: Render templte

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,7 +12,7 @@ def basic_route() -> str:
     """
     Basic route
     """
-    return render_template("0-index.html")
+    return render_template("0-index.html",)
 
 
 if __name__ == "__main__":
